ee24d861b6ddab5ecbaf7a15d9f0a5c8f6f33223
Simon <113917705+cossinsim@users.noreply.github.com>
2022-10-15 23:24:34 +0200

    Débug supp. + affichage de suggestions code ISO3

--- v3_React_DEBUGGING/src/components/App.js
+++ v3_React_DEBUGGING/src/components/App.js
@@ -1,26 +1,41 @@
 import { React, useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
-//import TableIso3 from "./TableIso3.js";
 import "./App.css";
 
 function App() {
 
   const [APIData, setAPIData] = useState([]);
   const [isoFilter, setIsoFilter] = useState([]);
-  const [searchInput, setSearchInput] = useState('');
-  const [error, setError] = useState({message: null,});
+  const [searchInput, setSearchInput] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [isoHint, setIsoHint] = useState([]);
+  const [error, setError] = useState(null);
   const [selectedValue, setSelectedValue] = useState("");
   const [matchingIso, setMatchingIso] = useState("");
+  const [tableClassSwitcher, setTableClassSwitcher] = useState("");
 
-  const searchApi = (iso, indicator) => {
-    fetch(`http://api.worldbank.org/v2/country/${iso}/indicator/${indicator}?format=json`)
-          .then(response => response.json())
-          .then((response) => {
-              setAPIData(response[1]);
-          })
+  // FONCTIONS  
+
+  // GESTION CHANGEMENT INDICATOR
+  const indicatorChange = (value) => {
+    setSelectedValue(value);
+    console.log(selectedValue);
   }
 
-  // RENDER COMPOSANT -> INIT. COMPARATEUR
+  // VERIF CARACS SPECIAUX 
+  const noSpecialChars = (searchValue) => {
+    setInputValue(searchValue.replace(/[^a-zA-Z0-9]/g, ""))
+  }
+
+  // FORMATAGE USD 
+  const dollarUSLocale = new Intl.NumberFormat('en-US', {
+    style: "currency",
+    currency: "USD",
+    currencydisplay: "symbol",
+  });
+
+  // USEEFFECTS
+  // RENDER COMPOSANT (()=> {...} ,[]) -> INIT. COMPARATEUR
   useEffect(() => {
     fetch(`https://api.worldbank.org/v2/country?per_page=1000&format=json`)
         .then(response => response.json())
@@ -28,61 +43,85 @@ function App() {
             //console.log(response[1]);
             setIsoFilter(response[1].map( ({ id, name })  => ({id, name}) ));
         })
-}, [])
+  }, [])
 
-// USEEFFECT RESOUT LE PB DE DESYNCHRO, QD CONDITION
-// REMPLIE, MAJ AFFICHAGE 
-useEffect(()=> {
-  if (matchingIso !== "" && selectedValue !== "") {
-    searchApi(matchingIso, selectedValue);
-    console.log(matchingIso);
-    console.log(selectedValue);
-  }
-}, [matchingIso, selectedValue]);
+  // USEEFFECT RESOUT LE PB DE DESYNCHRO, QD CONDITION
+  // REMPLIE, MAJ AFFICHAGE 
+  useEffect(()=> {
+    // REQUETE API AVEC ISO MATCHE + INDICATOR SELECTIONNE
+    const searchApi = (iso, indicator) => {
+      try {
+        fetch(`http://api.worldbank.org/v2/country/${iso}/indicator/${indicator}?format=json`)
+        .then(response => response.json())
+        .then((response) => {
+            setAPIData(response[1]);
+        })
+      } catch (error) {
+        setError(error)
+      }
 
-useEffect(()=> {
-  if (searchInput !== '') {
-    const filteredData = isoFilter.filter((item) => {
-        return item.id.toLowerCase().includes(searchInput.toLowerCase()) 
-    })
-    //console.log(isoFilter);
-    //console.log(filteredData);
-    console.log(filteredData[0].id);
-    setMatchingIso(`${filteredData[0].id}`);
-    //console.log(matchingIso);
-}
-}, [searchInput]);
-
-
-  const indicatorChange = (value) => {
-      setSelectedValue(value);
-      console.log(selectedValue);
-    //
-  }
-
-  // FONCTION VERIF LONGUEUR INPUT 
-  // ET SET STATE setSearchInput
-  const searchItems = (searchValue) => {
-    console.log(searchValue);
-    console.log(searchValue.length);
-    if (searchValue.length === 3) {
-      console.log("TEST LONGUEUR 3");
-      setSearchInput(searchValue);
+      if (error) {
+        console.dir(error);
+      }
     }
-    //
-}
+
+    if (matchingIso !== "" && selectedValue !== "") {
+      searchApi(matchingIso, selectedValue);
+
+      // AFFICHAGE "LONG" SI REQUETE EFFECTUEE 
+      setTableClassSwitcher("table-display-data");
+      console.log(matchingIso);
+      console.log(selectedValue);
+    }
+  }, [matchingIso, selectedValue, /*error*/]);
+
+  useEffect(()=> {
+    if (searchInput !== '') {
+      const filteredData = isoFilter.filter((item) => {
+          return item.id.toLowerCase().includes(searchInput.toLowerCase()) 
+      })
+      console.log(filteredData[0].id);
+
+      // ICI !!! FIX SI AUCUN MATCH TROUVE
+      setMatchingIso(`${filteredData[0].id}`); 
+      // ICI !!! FIX SI AUCUN MATCH TROUVE
+
+      //console.log(matchingIso);
+  }
+  }, [searchInput]);
+
+  // SET STATE setSearchInput SI LENGTH INPUT === 3
+  useEffect(()=> {
+    if (inputValue.length === 3) {
+      //setTableClassSwitcher("table-display-data");
+      setSearchInput(inputValue);
+    }
+    else {
+      //setTableClassSwitcher("table-display-hint");
+      const filteredIsoHint = 
+      isoFilter.filter(el => {
+        if (inputValue === "") {
+          return el;
+        }
+        else {
+          return el.id.toLowerCase().includes(inputValue.toLowerCase());
+        }
+    });
+    setTableClassSwitcher("table-display-hint");
+    setIsoHint(filteredIsoHint);
+    }
+  }, [inputValue]);
 
 return (
-  // UNE DIV EST DEJA RENDER DANS root.render (index.js) !
-  <div className="main">
+  <section className="app-section">
       <TextField 
+          value={inputValue}
           id="outlined-basic"
-          onChange={e => searchItems(e.target.value)}
-          label="Search"
+          className="search"
+          onChange={e => noSpecialChars(e.target.value)}
           variant="outlined"
-          fullWidth
-          placeholder='Search...'
-          inputProps={{ maxLength: 3}}
+          placeholder='Enter ISO 3 code'
+          inputProps={{ maxLength: 3, style: {fontSize: 20, height: 10} }}
       />
 
       <label htmlFor="select-indicator" hidden>Select an indicator</label>
@@ -91,13 +130,14 @@ return (
         onChange={e => indicatorChange(e.target.value)}
         name="select-indicator"
       >
-        <option value="">PLACEHOLDER</option>
+        <option value="">Please select an indicator :</option>
         <option value="NY.GDP.MKTP.CD">GDP - ... </option>
         <option value="NE.IMP.GNFS.CD">Imports of goods and services</option>
         <option value="NY.ADJ.NNTY.PC.CD">Adjusted net national income per capita</option>
       </select>
 
-      <table>
+      <div className="testClass">
+      <table className={tableClassSwitcher}>
         <tbody>
           {searchInput.length === 3 ? (
               APIData.map((item) => {
@@ -115,13 +155,13 @@ return (
                   return (
                     <tr key={item.date}>
                       <td>{item.date}</td>
-                      <td>{item.value}</td>
+                      <td>{dollarUSLocale.format(item.value)}</td>
                     </tr>
                   )
                 }
               })
           ) : (
-              isoFilter.map((item) => {
+              isoHint.map((item) => {
                   return (
                     <tr key={item.id}>
                       <td>{item.id}</td>
@@ -132,7 +172,8 @@ return (
           )}
         </tbody>
       </table>
-  </div>
+      </div>
+    </section>
 )
 }
 
